d2bcb594f927f33caaf3947c0c460e77fdb67f7c
Pochobs <pochobs@hotmail.com>
2020-06-26 13:02:08 -0500

    se agrego el loop en la llamada de la funcion

--- assets/js/robot.js
+++ assets/js/robot.js
@@ -4,55 +4,49 @@ var apodoAire = 100;
 var apodoEgo = 15;
 var cachetada = 40;
 var patada = 55;
-var golpe = [" la nariz ", " el diente ", " el brazo ", " la oreja " , " el ojo "]
-for( var i = 0; i < golpe.length; i++){
-    console.log(golpe[i]);
-}
-    var quechi = golpe[i];
-    console.log(quechi)
-    var fight = function() {
+var golpe = [" la nariz ", " el diente ", " el brazo ", " la oreja " , " el ojo "];
+
+    var fight = function(golpe) {
         // repeat and execute as long as the enemy robot is alive 
         
-        while(apodoAire => 0 || apodoAire <=200) {
+        while(apodoAire => 0 || apodoAire <=200 || golpe++) {
             window.alert("pasan 0.5 min " + apodo + " en su maximo apojeo empieza a escribir: Bla, bla, Bla y se infla su ego en 15 puntos");
             apodoAire = apodoAire + apodoEgo;
             window.alert( apodo + " se infló y ahora tiene " +  apodoAire + " puntos de aire");
             promptFight = window.prompt("escoge entre 'A' para una triple cachetada ó 'B' para una patada voladora, que hará que desinfle el ego de " + apodo);
             if (promptFight === "a" || promptFight === "A") {
                 apodoAire = apodoAire - cachetada;
-                for( var i = 0; i < golpe.length; i++){
-                    var quechi = golpe[i];
                 
-                window.alert("le rompiste " + quechi  + " y ahora tiene " + apodoAire + " de aire."); 
-                }
+                
+                window.alert("le rompiste " + golpe  + " y ahora tiene " + apodoAire + " de aire."); 
+                
                 
                     if (apodoAire <= 0) {
-                        window.alert(apodo + " se churreteo");
+                        window.alert(apodo + " es un 'Cholo Terco' no se quiere callar");
                         break;
                     } 
                     else if (apodoAire > 200){
-                        window.alert(apodo + " QUEMA CEREBRO Y EXPLOTA");
+                        window.alert(apodo + " QUEMO CEREBRO Y EXPLOTA");
                         break;
                     }
                     else {
                         window.alert(apodo + " tiene ganas de seguir jodiendo.");
-                        
+                        golpe;
                    
-                            fight();
+                           
                         }
                       
             }
             else if (promptFight === "b" || promptFight === "B") {
                 
                 apodoAire = apodoAire - patada;
-                for( var i = 0; i < golpe.length; i++){
-                    var quechi = golpe[i];
-                   
-                window.alert("le rompiste " + quechi  + " y ahora tiene " + apodoAire + " de aire."); 
-                }
+                
+               console.log(golpe);
+                window.alert("le rompiste " + golpe  + " y ahora tiene " + apodoAire + " de aire."); 
+               
                 
                     if (apodoAire <= 0) {
-                        window.alert(apodo + " se churreteo");
+                        window.alert(apodo + " es un 'Cholo Terco' no se quiere callar");
                         break;
                     } 
                     else if (apodoAire > 200){
@@ -61,17 +55,19 @@ for( var i = 0; i < golpe.length; i++){
                     }
                     else {
                         window.alert(apodo + " tiene ganas de seguir jodiendo.");
-                        
+                        golpe;
                    
-                            fight();
+                           
                         }
                      
             }
             else {
-                window.alert("You need to pick a valid option. Try again!");
+                window.alert("es 'A' o 'B'" );
             }
         }
     }
         
-        
-            fight();
+    for( var i = 0; i < golpe.length; i++){
+        debugger;
+           fight(golpe[i]);
+        }
